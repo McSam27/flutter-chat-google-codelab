156786e03d98943aa5edb9b592dda86a1940e773
Sam McCagg <sam.mccagg@ellucian.com>
2018-12-09 13:50:06 -0500

    split the components out into modular code.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,26 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(new MaterialApp(
+  runApp(new FriendlychatApp());
+}
+
+// build method returns the actual UI to render to the screen
+class FriendlychatApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
       title: "Friendlychat",
-      home: new Scaffold(appBar: new AppBar(title: new Text("Friendlychat")))));
+      home: new ChatScreen(), // render new ChatScreen UI class component
+    );
+  }
+}
+
+// build method returns the actual UI to render to the screen
+class ChatScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(title: new Text('Friendlychat'))
+    );
+  }
 }
