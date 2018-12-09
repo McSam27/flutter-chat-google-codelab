91729681bc1f918cfcd60f9b56b332f99672441b
Sam McCagg <sam.mccagg@ellucian.com>
2018-12-09 13:44:17 -0500

    init commit.

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(new MaterialApp(
+      title: "Friendlychat",
+      home: new Scaffold(appBar: new AppBar(title: new Text("Friendlychat")))));
+}
